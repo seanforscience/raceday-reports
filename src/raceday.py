706617c2aebbd78060b0a4ce8be1a4f7b9072b46
Sean <sean@probablyridiculous.com>
2020-03-31 17:17:56 -0700

    included the raceday class.

---
+++ src/raceday.py
@@ -0,0 +1,50 @@
+import pandas as pd
+
+class RaceDay():
+    def __init__( self , exportPath ):
+        
+        self.exportPath = exportPath
+        self.data = self.loadExport()
+        
+        self.entrants = self.getEntrantsList(self.data)
+        self.shirts = self.getShirts(self.data)
+    
+    def loadExport( self ):
+
+        standardize = lambda text : text.lower().replace("  "," ").replace("  "," ").replace(" ","_")
+    
+        rawExport = pd.read_csv( self.exportPath )
+        rawExport.columns = [standardize(column) for column in rawExport.columns]
+        rawExport["bib"] = rawExport["bib"].fillna(-1).apply(int)
+    
+        return(rawExport)
+    
+    def getEntrantsList( self , data ):
+    
+        entrants = data[["bib","first_name","last_name","city","state"]].copy()
+    
+        return(entrants)
+    
+    def getShirts( self , data ):
+
+        shirtColumns = [c for c in data.columns if "mens" in c]
+        nameColumns = ["bib","first_name","last_name"]
+        shirts = data[ nameColumns + shirtColumns].copy().fillna(0)
+
+    
+        totals = shirts[shirtColumns].sum()
+        shirts.loc["totals"] = totals
+    
+        # clean up
+        shirts[shirtColumns] = shirts[shirtColumns].applymap(int)
+        shirts[nameColumns] = shirts[nameColumns].fillna("totals")
+
+    
+        return(shirts)
+
+    def export( self , path ):
+        
+        self.shirts.to_csv( path + "shirts.csv")
+        self.entrants.to_csv( path + "entrants.csv")        
+
+
